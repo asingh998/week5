8416d788625b8655aa421e85a61424a25206943c
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-05-04 17:51:42 -0400

    added content

--- index.php
+++ index.php
@@ -11,11 +11,32 @@ require_once("vendor/autoload.php");
 $f3 = Base::instance();
 
 //Default route
-$f3->route('GET /', function() {
-    echo '<h1>week5</h1>';
+$f3->route('GET /', function($f3) {
+    //echo '<h1>Week 5</h1>';
 
-    //$views = new Template();
-    //echo $views->render('views/home.html');
+    //Create variables in the F3 hive
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('temp', 67);
+
+    //Define an array of fruits
+    $f3->set('fruits', array('apple', 'orange', 'banana'));
+
+    //Define an associative array of desserts
+    $f3->set('desserts', array(
+        'chocolate'=>'Chocolate Mousse',
+        'vanilla'=>'Vanilla Custard',
+        'strawberry'=>'Strawberry Shortcake'
+    ));
+
+    //conditional content
+    $f3->set('preferredCustomer', true);
+    $f3->set('lastLogin', strtotime('-1 week'));
+
+    //Display the template/view
+    $view = new Template();
+    echo $view->render('views/info.html');
 });
 
 //Run F3
